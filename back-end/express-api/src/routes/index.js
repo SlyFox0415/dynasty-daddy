99a7e720fac5e1e84ac595001eeb9cb7819b8e01
alltimehappyboy <victorman@skiff.com>
2024-02-27 02:23:58 -0500

    fix eorros on routes

--- back-end/express-api/src/routes/index.js
+++ back-end/express-api/src/routes/index.js
@@ -19,7 +19,7 @@ import {
   // GetFFLeagueEndpoint,
   GetFFLeagueTransactionsEndpoint,
   GetFFRostersEndpoint,
-  GetFFScheduleEndpoint,
+  // GetFFScheduleEndpoint,
   GetFFStandingsEndpoint,
   GetFFTeamDraftPicksEndpoint,
   GetFFTradesEndpoint,
